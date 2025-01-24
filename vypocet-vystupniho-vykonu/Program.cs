0fafb4c74e82940164783c12fd17b92649d87378
Karolína Latislavová <karolina.latis.022@pslib.cz>
2025-01-24 13:23:30 +0100

    osetreni

--- vypocet-vystupniho-vykonu/Program.cs
+++ vypocet-vystupniho-vykonu/Program.cs
@@ -10,15 +10,40 @@ class Program
 
 		// zadání výstupního výkonu od uživatele
 		Console.WriteLine("Zadejte vstupní výkon signálu Pin [mW]: ");
-		double vstupniVykon = double.Parse(Console.ReadLine());  // P [mW]
+
+        double vstupniVykon = 0; //inicialnize promenne
+		double.TryParse(Console.ReadLine(), out vstupniVykon); // [mW]
+
 
 		// zadání zisku zesilovače nebo filtru od uživatele
+		double ziskNeboUtlumdB = 0;
 		Console.WriteLine("Zadejte zisk zesilovače nebo útlum filtru (G) [dB]: ");
-		double ziskNeboUtlum = double.Parse(Console.ReadLine()); // G [dB]
+
+
+		//osetreni vyjimky
+		try
+		{
+
+			ziskNeboUtlumdB = double.Parse(Console.ReadLine()); // G [dB]
+
+		}
+
+		catch (OverflowException)
+		{
+			Console.WriteLine("Neplatný vstup (overflowexeption");
+
+		}
+		catch (Exception)
+		{
+			Console.WriteLine("Neplatný vstup.");
+
+		}
+
+
 
 		// Pout = 0,005*10 ^ (23 /10) = 1 W
 		// zesílení nebo útlum (poměr)
-		double pomer = Math.Pow(10, ziskNeboUtlum / 10);
+		double pomer = Math.Pow(10, ziskNeboUtlumdB / 10);
 
 		//výpočet výstupního výkonu
 		double vystupniVykon = vstupniVykon * pomer;
@@ -26,8 +51,11 @@ class Program
 		//výstupy pro uživatele
 		Console.WriteLine("\nVýsledky:");
 
-		Console.WriteLine($"Zisk nebo útlum (poměr): {pomer:F4}");
-		Console.WriteLine($"Výstupní výkon Pout):{vystupniVykon:F4} [mW]");
+		Console.WriteLine($"Zisk nebo útlum: {ziskNeboUtlumdB:F2} [dB]");
+		Console.WriteLine($"Výstupní výkon Pout:{vystupniVykon:F2} [mW]");
+
+		Console.WriteLine("\nVýsledky:");
+		Console.WriteLine($"Zisk nebo utlum pomer: {pomer:F2}");
 
 
 		Console.WriteLine("\nStiskněte Enetr pro ukončení programu");
